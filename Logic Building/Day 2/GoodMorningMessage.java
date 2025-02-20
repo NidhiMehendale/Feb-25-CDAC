243498a9b149352d7ef5349a3d3368627e982c68
Nidhi <48858306+NidhiMehendale@users.noreply.github.com>
2025-02-20 14:53:21 +0530

    Add files via upload

---
+++ Logic Building/Day 2/GoodMorningMessage.java
@@ -0,0 +1,10 @@
+ class GoodMorningMessage  {
+    public static void main(String[] args) {
+        // Predefined time (in 24-hour format)
+        int hour = 9;
+
+        if (hour >= 5 && hour < 12) {
+            System.out.println("Good Morning!");
+        }
+    }
+}
