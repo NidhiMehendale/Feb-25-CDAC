bea617f09bb723de086a4a08984ffbe60129f11e
Nidhi <48858306+NidhiMehendale@users.noreply.github.com>
2025-03-14 21:53:39 +0530

    Add files via upload

---
+++ OOPJ/Assignment2/Q25.java
@@ -0,0 +1,16 @@
+//Implement a program that reverses an integer number without using string conversion (StringBuilder or toCharArray).
+import java.util.Scanner;
+class Q25{
+	 public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number:");
+		int n = sc.nextInt();
+		int rev = 0;
+		int temp = n;
+		while(n!=0){
+			rev = rev * 10 + n % 10;
+			n = n/10;
+		}
+		System.out.println("Reverse of " + temp + " is " + rev);
+	}
+}
